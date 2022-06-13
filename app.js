b2c429f9e116b143ae964c3c1a17e1ef69f00546
nimplay <nimrod7day@gmail.com>
2022-06-13 19:31:58 -0400

    fixing linters

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-function toggleMenu () {
+function toggleMenu ( ) {
   const menuToggle = document.querySelector('.burger');
   const menuActive = document.querySelector('.sidebar');
-  const logoActive = document.querySelector('.logo');    
+  const logoActive = document.querySelector('.logo');
   menuToggle.classList.toggle('active');
   menuActive.classList.toggle('active');
   logoActive.classList.toggle('active');
