5e719d2403281cef252f9652489876e732921377
nimplay <nimrod7day@gmail.com>
2022-06-15 16:36:54 -0400

    fixing linters

--- app.js
+++ app.js
@@ -1,55 +1,55 @@
 /* Object */
 const projectData = [
   {
-    title: "Data Dashboard Healthcare",
-    lenguage1: "HTML",
-    lenguage2: "Boostrap",
-    lenguage3: "Ruby",
-    image: "./Images/Card1.png",
-    imageAlt: "SnapshootPortfolio",
+    title: 'Data Dashboard Healthcare',
+    lenguage1: 'HTML',
+    lenguage2: 'Boostrap',
+    lenguage3: 'Ruby',
+    image: './Images/Card1.png',
+    imageAlt: 'SnapshootPortfolio',
     description:
       "daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
   },
 
   {
-    title: "Website Protfolio ",
-    lenguage1: "HTML",
-    lenguage2: "Boostrap",
+    title: 'Website Protfolio ',
+    lenguage1: 'HTML',
+    lenguage2: 'Boostrap',
     lenguage3: "Ruby",
-    image: "./Images/SnapshootPortfolio.png",
-    imageAlt: "SnapshootPortfolio",
+    image: './Images/SnapshootPortfolio.png',
+    imageAlt: 'SnapshootPortfolio',
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
   },
 
   {
-    title: "Profesional Art Printing Data More",
-    lenguage1: "HTML",
-    lenguage2: "JavaScript",
-    lenguage3: "CSS",
-    image: "./Images/SnapshootPortfolio.png",
-    imageAlt: "SnapshootPortfolio",
+    title: 'Profesional Art Printing Data More',
+    lenguage1: 'HTML',
+    lenguage2: 'JavaScript',
+    lenguage3: 'CSS',
+    image: './Images/SnapshootPortfolio.png',
+    imageAlt: 'SnapshootPortfolio',
     description:
       "daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standar",
   },
 
   {
-    title: "Data Dashboard Healthcare",
-    lenguage1: "Ruby",
-    lenguage2: "bootstrap",
-    lenguage3: "Ruby on Rails",
-    image: "./Images/Mask.png",
-    imageAlt: "SnapshootPortfolio",
+    title: 'Data Dashboard Healthcare',
+    lenguage1: 'Ruby',
+    lenguage2: 'bootstrap',
+    lenguage3: 'Ruby on Rails',
+    image: './Images/Mask.png',
+    imageAlt: 'SnapshootPortfolio',
     description:
       "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
   },
   {
-    title: "Profesional Art Printing Data More",
-    lenguage1: "HTML",
-    lenguage2: "JavaScript",
-    lenguage3: "CSS",
-    image: "./Images/SnapshootPortfolio.png",
-    imageAlt: "SnapshootPortfolio",
+    title: 'Profesional Art Printing Data More',
+    lenguage1: 'HTML',
+    lenguage2: 'JavaScript',
+    lenguage3: 'CSS',
+    image: './Images/SnapshootPortfolio.png',
+    imageAlt: 'SnapshootPortfolio',
     description:
       "daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standar",
   },
@@ -109,25 +109,21 @@ createPopUp.addEventListener("click", () => {
   </div>
 `;
   function cardDefault(projectData, index) {
-    document.querySelector(".pop-up-title").textContent =
-      projectData[index].title;
-    document.querySelector("#lenguage1").textContent =
-      projectData[index].lenguage1;
-    document.querySelector("#lenguage2").textContent =
-      projectData[index].lenguage2;
-    document.querySelector("#lenguage3").textContent =
-      projectData[index].lenguage3;
-    document.querySelector(".pop-up-image").src = projectData[index].image;
-    document.querySelector(".pop-up-image").alt = projectData[index].imageAlt;
-    document.querySelector(".pop-up-p").textContent =
+    document.querySelector('.pop-up-title').textContent = projectData[index].title;
+    document.querySelector('#lenguage1').textContent = projectData[index].lenguage1;
+    document.querySelector('#lenguage2').textContent = projectData[index].lenguage2;
+    document.querySelector('#lenguage3').textContent = projectData[index].lenguage3;
+    document.querySelector('.pop-up-image').src = projectData[index].image;
+    document.querySelector('.pop-up-image').alt = projectData[index].imageAlt;
+    document.querySelector('.pop-up-p').textContent =
       projectData[index].description;
   }
   cardDefault(projectData, index);
-  const closePop = document.querySelector(".pop-up-x");
+  const closePop = document.querySelector('.pop-up-x');
   closePop.addEventListener("click", () => {
     pop.removeChild(section);
   });
-  const nextPop = document.querySelector("#arrow1");
+  const nextPop = document.querySelector('#arrow1');
   nextPop.addEventListener("click", () => {
     if (index > 0 && index < cards) {
       index -= 1;
@@ -137,7 +133,7 @@ createPopUp.addEventListener("click", () => {
       cardDefault(projectData, index);
     }
   });
-  const nextPop2 = document.querySelector("#arrow2");
+  const nextPop2 = document.querySelector('#arrow2');
   nextPop2.addEventListener("click", () => {
     if (index < cards - 1 && index >= 0) {
       index += 1;
