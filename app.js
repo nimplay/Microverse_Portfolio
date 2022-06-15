4a058c84385976c661766c848b9fcbb21b57f35f
nimplay <nimrod7day@gmail.com>
2022-06-15 16:06:29 -0400

    Adding blur and creating carrusel cards

--- app.js
+++ app.js
@@ -1,23 +1,57 @@
 /* Object */
 const projectData = [{
-  title: 'Project Title',
+  title: 'Data Dashboard Healthcare',
+  lenguage1: 'HTML',
+  lenguage2: 'Boostrap',
+  lenguage3: 'Ruby',
+  image: './Images/Card1.png',
+  imageAlt: 'SnapshootPortfolio',
+  description: "daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard"
+}, 
+
+{
+  title: 'Website Protfolio ',
+  lenguage1: 'HTML',
+  lenguage2: 'Boostrap',
+  lenguage3: 'Ruby',
+  image: './Images/SnapshootPortfolio.png',
+  imageAlt: 'SnapshootPortfolio',
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard" 
+}, 
+
+{
+  title: 'Profesional Art Printing Data More',
   lenguage1: 'HTML',
   lenguage2: 'JavaScript',
   lenguage3: 'CSS',
   image: './Images/SnapshootPortfolio.png',
   imageAlt: 'SnapshootPortfolio',
-  description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur amet quasi suscipit enim quidem quas odio libero totam nisi. Numquam corrupti sequi eligendi error laboriosam quasi nulla quidem eaque reiciendis.',
-},
+  description: "daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standar"
+}, 
 
 {
-  title: 'Project Title 2',
+  title: "Data Dashboard Healthcare",
   lenguage1: 'Ruby',
   lenguage2: 'bootstrap',
   lenguage3: 'Ruby on Rails',
+  image: './Images/Mask.png',
+  imageAlt: 'SnapshootPortfolio',
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard"
+},
+
+{
+  title: 'Profesional Art Printing Data More',
+  lenguage1: 'HTML',
+  lenguage2: 'JavaScript',
+  lenguage3: 'CSS',
   image: './Images/SnapshootPortfolio.png',
   imageAlt: 'SnapshootPortfolio',
-  description: 'Second Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur amet quasi suscipit enim quidem quas odio libero totam nisi. Numquam corrupti sequi eligendi error laboriosam quasi nulla quidem eaque reiciendis.',
+  description: "daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standar"
 }];
+
+let index = 0;
+let cards = projectData.length;
+
 /* Functions */
 function toggleMenu() {
   const menuToggle = document.querySelector('.burger');
@@ -36,47 +70,71 @@ createPopUp.addEventListener('click', () => {
   pop.appendChild(section);
   section.innerHTML = `
   <div class="pop-up">
-<div class="pop-up-box1">
-  <div class="pop-up-box-title">
-    <h1 class="pop-up-title"></h1>
-    <div class="pop-up-x"></div>
-  </div>        
-  <ul class="pop-up-ul">
-    <li class="pop-up-li" id="lenguage1">html</li>
-    <li class="pop-up-li" id="lenguage2">Bootstrap</li>
-    <li class="pop-up-li" id="lenguage3">Ruby on rails</li>
-  </ul>
-</div>
-<div class="pop-up-box2">
-  <img class="pop-up-image" src="" alt="SnapshootPortfolio">
-  <div class="pop-up-secundary-box1">
-    <p class="pop-up-p">
-    </p>
-    <div class="pop-up-secundsry-box2">
-      <a class="pop-up-button" href="https://nimplay.github.io/Microverse_Portfolio/#">
-        <p>See Live</p>
-        <img src="/Images/liveServer.svg" alt="">
-      </a>
-      <a class="pop-up-button" href="https://github.com/nimplay/Microverse_Portfolio">
-        <p>See Source</p>
-        <img src="/Images/whiteGitHub.svg" alt="">
-      </a>
-      
-    </div>
+  <div class="pop-up-box1">
+    <div class="pop-up-box-title">
+      <h1 class="pop-up-title"></h1>
+      <div class="pop-up-x"></div>
+    </div>        
+    <ul class="pop-up-ul">
+      <li class="pop-up-li" id="lenguage1"></li>
+      <li class="pop-up-li" id="lenguage2"></li>
+      <li class="pop-up-li" id="lenguage3"></li>
+    </ul>
   </div>
-</div>
-</div>
+  <div class="pop-up-box2">
+      <div class="pop-up-arrow" id="arrow1"></div>
+    <img class="pop-up-image" src="">
+    <div class="pop-up-secundary-box1">
+      <p class="pop-up-p">
+      </p>
+      <div class="pop-up-secundsry-box2">
+        <a class="pop-up-button" href="https://nimplay.github.io/Microverse_Portfolio/#">
+          <p>See Live</p>
+          <img src="./Images/liveServer.svg" alt="">
+        </a>
+        <a class="pop-up-button" href="https://github.com/nimplay/Microverse_Portfolio">
+          <p>See Source</p>
+          <img src="./Images/whiteGitHub.svg" alt="">
+        </a>              
+      </div>
+    </div>
+    <div class="pop-up-arrow" id="arrow2"></div>
+  </div>
+  </div>
 `;
-  document.querySelector('.pop-up-title').textContent = projectData[1].title;
-  document.querySelector('#lenguage1').textContent = projectData[0].lenguage1;
-  document.querySelector('#lenguage2').textContent = projectData[1].lenguage2;
-  document.querySelector('#lenguage3').textContent = projectData[0].lenguage3;
-  document.querySelector('.pop-up-image').src = projectData[0].image;
-  document.querySelector('.pop-up-image').alt = projectData[0].imageAlt;
-  document.querySelector('.pop-up-p').textContent = projectData[0].description;
+function cardDefault(projectData, index) {
+document.querySelector('.pop-up-title').textContent = projectData[index].title;
+document.querySelector('#lenguage1').textContent = projectData[index].lenguage1;
+document.querySelector('#lenguage2').textContent = projectData[index].lenguage2;
+document.querySelector('#lenguage3').textContent = projectData[index].lenguage3;
+document.querySelector('.pop-up-image').src = projectData[index].image;
+document.querySelector('.pop-up-image').alt = projectData[index].imageAlt;
+document.querySelector('.pop-up-p').textContent = projectData[index].description;
+}
+cardDefault(projectData, index);
   const closePop = document.querySelector('.pop-up-x');
   closePop.addEventListener('click', () => {
     pop.removeChild(section);
+  });
+  const nextPop = document.querySelector('#arrow1');
+  nextPop.addEventListener('click', () => {   
+    if(index > 0 && index < cards){
+    index -= 1; 
+    cardDefault(projectData, index);
+    } else{
+    index = cards - 1;     
+    cardDefault(projectData, index);
+    } 
+  });
+  const nextPop2 = document.querySelector('#arrow2');
+  nextPop2.addEventListener('click', () => {
+  if(index < cards - 1 && index >= 0){
+      index += 1;       
+      cardDefault(projectData, index);
+    } else{
+      index = 0;     
+      cardDefault(projectData, index);
+    } 
   });
 });
 module.exports = toggleMenu();
